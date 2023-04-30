06a506f3ff63f0ba7847f06247d7c93dad134317
CarlaSlattery <cslatterydev@gmail.com>
2023-04-30 15:29:32 +0100

    set up page routes for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
- 
-
   return (
-    <div>
-    <h1>Surreal Estate</h1>
-    
-    </div>
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
