382737ce027edcba0106667162624c0ac9fcfe07
CarlaSlattery <cslatterydev@gmail.com>
2023-05-04 11:59:56 +0100

    sign-in and register forms added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,83 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io";
+import { AiOutlineEye } from "react-icons/ai";
+
 function Register() {
-  return <h1>Register</h1>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const { name, email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+
+        <form>
+          <input
+            type="text"
+            className="nameInput"
+            placeholder="Name"
+            id="name"
+            value={name}
+            onChange={onChange}
+          ></input>
+          <input
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          ></input>
+
+          <div className="passwordInput">
+            <input
+              type={showPassword ? "text" : "password"}
+              className="passwordInput"
+              placeholder="password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+            <AiOutlineEye
+              className="showPassword"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+          <Link to="/forgot-password" className="forgotPasswordLink">
+            Forgot Password
+          </Link>
+          <div className="registerBar">
+            <p className="registerText">Register</p>
+            <button className="registerButton">
+              <IoIosArrowForward fill="black" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+        {/*Google OAuth */}
+
+        <Link to="/sign-in" className="registerLink">
+          Sign In Instead
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default Register;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,68 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io";
+import { AiOutlineEye } from "react-icons/ai";
+
 function SignIn() {
-  return <h1>Sign In</h1>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+setFormData((prevState) => ({...prevState, [e.target.id]: e.target.value,}))
+  };
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+
+        <form>
+          <input
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          ></input>
+
+          <div className="passwordInput">
+            <input
+              type={showPassword ? "text" : "password"}
+              className="passwordInput"
+              placeholder="password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+            <AiOutlineEye
+              className="showPassword"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+          <Link to="/forgot-password" className="forgotPasswordLink">
+            Forgot Password
+          </Link>
+          <div className="signInBar">
+            <p className="signInText">Sign In</p>
+            <button className="signInButton">
+              <IoIosArrowForward fill="black" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+        {/*Google OAuth */}
+
+        <Link to="/register" className="registerLink">
+          Sign Up Instead
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default SignIn;
